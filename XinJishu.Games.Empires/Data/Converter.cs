d9f0f7d64851f9bf6aa316d9acba601369b7a252
pvlamm <lammster@gmail.com>
2014-10-30 23:28:04 -0400

    Started adding some db related code

---
+++ XinJishu.Games.Empires/Data/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XinJishu.Games.Empires.Models;
+
+namespace XinJishu.Games.Empires.Data
+{
+    public static class Converter
+    {
+        public static Galaxy ToGalaxy(DataRow dr)
+        {
+            Galaxy g = new Galaxy();
+
+            g.galaxy_shape = (GalaxyShape)Enum.Parse(typeof(GalaxyShape), dr["Shape"].ToString(), true);
+            g.id = Convert.ToInt32(dr["id"]);
+            g.name = Convert.ToString(dr["name"]);
+
+            return g;
+        }
+    }
+}
---
+++ XinJishu.Games.Empires/Data/DataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XinJishu.Games.Empires.Data
+{
+    public class DataSource : XinJishu.Data.SQLServer.ConnectionManager
+    {
+        private Int32 galaxy_id { get; set; }
+
+
+        public DataSource(String connection_string)
+            : base(connection_string)
+        {
+
+        }
+
+        public 
+    }
+}
