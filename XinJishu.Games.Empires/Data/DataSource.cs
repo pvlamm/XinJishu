d6a43ea190d7a60704d5891f7aad6c5894c4328c
pvlamm <lammster@gmail.com>
2014-10-31 21:56:46 -0400

    checkin

--- XinJishu.Games.Empires/Data/DataSource.cs
+++ XinJishu.Games.Empires/Data/DataSource.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XinJishu.Games.Empires.Models;
 
 namespace XinJishu.Games.Empires.Data
 {
@@ -11,12 +12,21 @@ namespace XinJishu.Games.Empires.Data
         private Int32 galaxy_id { get; set; }
 
 
-        public DataSource(String connection_string)
+        public DataSource(String connection_string, Int32 galaxy_id)
             : base(connection_string)
         {
-
+            this.galaxy_id = galaxy_id;
         }
 
-        public 
+        public IEnumerable<Galaxy> GetGalaxies()
+        {
+            using (var cmd = this.GetCommand())
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "[dbo].[Galaxies_Select]";
+                
+                
+            }
+        }
     }
 }
--- XinJishu/Logging.cs
+++ XinJishu/Logging.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace XinJishu
 {
-    class Logging
+    public class Logging
     {
     }
 }
