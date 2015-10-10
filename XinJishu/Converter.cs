6ec780852426fbbe58c4354a431b70d18433fd96
icewolf1911 <lammster@gmail.com>
2015-10-10 05:22:45 -0400

    added Converter static as a sister/friend to Convert

---
+++ XinJishu/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XinJishu
+{
+    public static class Converter
+    {
+        public static Int32 ToInt32(Object o)
+        {
+            var t = o.ToString();
+            Int32 r = default(Int32);
+
+            Int32.TryParse(t, out r);
+            
+            return r;
+        }
+
+        public static Guid ToGuid(Object o)
+        {
+            var s = o.ToString();
+            Guid g = Guid.Empty;
+
+            Guid.TryParse(s, out g);
+
+            return g;
+        }
+    }
+}
