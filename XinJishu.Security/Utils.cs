ff0d0515d72420381a9068d32200818c675c5340
Patrick Lamm <lammster@gmail.com>
2015-12-07 23:18:42 -0500

    Added random-string generator for nefarious reasons... maniacal laugh.... maniacal laugh... maniacal laugh

---
+++ XinJishu.Security/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XinJishu.Security
+{
+    public static class Utils
+    {
+        public static string GenerateRandomstring(int length)
+        {
+            if (length <= 0)
+                return string.Empty;
+
+            char[] chars = new char[52];
+            chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(); // 1234567890
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[length];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(length);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
