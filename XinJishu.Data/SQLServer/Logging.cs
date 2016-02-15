241990e6167365de8dbbac649c477c38e641ff96
Patrick Lamm <lammster@gmail.com>
2016-02-14 19:49:54 -0500

    Added the beginnings of a Logging manager in SQLServer

---
+++ XinJishu.Data/SQLServer/Logging.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XinJishu.Data.SQLServer
+{
+    public class Logging
+    {
+        private string connectionString { get; set; }
+        private string name { get; set; }
+        public Logging(string name, string connectionString) { }
+
+        public void Log(LoggingType type, string message)
+        {
+
+        }
+    }
+
+    public enum LoggingType
+    {
+        Information,
+        Important,
+        Warning,
+        Critical,
+        Unknown
+    }
+}
